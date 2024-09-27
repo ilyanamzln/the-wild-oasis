6b24d0150607e0968b07f767322fd66eed0d452a
ilyana <ilyanamazlan99@gmail.com>
2024-09-27 12:50:37 +0800

    feat: display dashboard statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -11,12 +13,25 @@ const StyledDashboardLayout = styled.div`
 `;
 function DashboardLayout() {
   const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
-  const { isLoading: isLoadingStays, stays, confirmedStays } = useRecentStays();
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { isLoading: isLoadingCabins, cabins } = useCabins();
 
-  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+  if (isLoadingBookings || isLoadingStays || isLoadingCabins)
+    return <Spinner />;
 
   return (
     <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
       <div>Statistics</div>
       <div>Today&apos;s Activity</div>
       <div>Chart Stay Durations</div>
